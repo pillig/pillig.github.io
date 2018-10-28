40223d6e35b7553e97d005aba1de80d8d4f2a736
Parker Illig <parker.illig@gmail.com>
2018-10-28 16:44:47 -0600

    Updates

---
+++ precache-manifest.44dd0efa671574d0ef15982ab163519d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cede3cee499de1ef786",
+    "url": "/static/js/main.8cede3ce.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "8cede3cee499de1ef786",
+    "url": "/static/css/main.0bd8620d.chunk.css"
+  },
+  {
+    "revision": "555200969e5c193cead633dbb2a4c5ec",
+    "url": "/index.html"
+  }
+];
