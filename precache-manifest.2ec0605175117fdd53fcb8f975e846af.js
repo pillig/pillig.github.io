929b22ccbca008e47d0a08268c8a0084342a2156
Parker Illig <parker.illig@gmail.com>
2018-10-29 00:07:57 -0600

    Updates

---
+++ precache-manifest.2ec0605175117fdd53fcb8f975e846af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d22bcd0d85100e123c48",
+    "url": "/static/js/main.d22bcd0d.chunk.js"
+  },
+  {
+    "revision": "c41476583f824f1fb007",
+    "url": "/static/js/1.c4147658.chunk.js"
+  },
+  {
+    "revision": "d22bcd0d85100e123c48",
+    "url": "/static/css/main.f1bdd060.chunk.css"
+  },
+  {
+    "revision": "c41476583f824f1fb007",
+    "url": "/static/css/1.7ca47fd1.chunk.css"
+  },
+  {
+    "revision": "4162588d3fd334f1b05b14155098ad88",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c70407b9338bf91a0d947d67f6b1e85.js"
+  "/precache-manifest.2ec0605175117fdd53fcb8f975e846af.js"
 );
 
 workbox.clientsClaim();
